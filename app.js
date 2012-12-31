867fa1433f3d8691b7bcae9a4c0cf8e8704644a6
Eric Ferraiuolo <eferraiuolo@gmail.com>
2012-12-30 22:07:58 -0500

    Configure app and create a basic view.

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 var express = require('express'),
+    hbs     = require('express-hbs'),
+    path    = require('path'),
+
+    middleware = require('./lib/middleware'),
 
     app = express();
 
 require('./config')(app);
 
+app.engine('hbs', hbs.express3({
+    defaultLayout: app.get('layout'),
+    partialsDir  : app.get('dirs').partials
+}));
+
+app.set('view engine', 'hbs');
+
+if (app.get('env') === 'development') {
+    app.use(express.logger('tiny'));
+}
+
+app.use(express.compress());
+app.use(express.favicon());
+app.use(express.static(app.get('dirs').pub));
+app.use(app.router);
+app.use(middleware.slash);
+
+if (app.get('env') === 'development') {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack     : true
+    }));
+} else {
+    app.use(express.errorHandler());
+}
+
 app.get('/', function (req, res) {
-    res.send('Hello!');
+    res.render('home');
 });
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,20 +1,28 @@
+var path = require('path'),
+
+    appRoot = process.cwd(),
+    env     = process.env;
+
 module.exports = function (app) {
-    var env = process.env;
+    var dirs = {
+        pub     : path.join(appRoot, 'public/'),
+        views   : path.join(appRoot, 'views/'),
+        partials: path.join(appRoot, 'views/partials/')
+    };
 
     app.set('name', 'Leslie-Eric Wedding');
-    app.set('title', 'Leslie’s & Eric’s Wedding');
 
-    app.set('dirs', {
-        pub     : 'public/',
-        views   : 'views/',
-        partials: 'views/partials/'
-    });
+    app.set('dirs', dirs);
+    app.set('views', dirs.views);
+    app.set('layout', path.join(dirs.views, 'layouts/main'));
 
-    app.set('layouts', {
-        main: 'layouts/main'
-    });
+    app.enable('strict routing');
 
     app.set('port', env.PORT || 5000);
     app.set('typekit', env.TYPEKIT);
     app.set('pictos', env.PICTOS);
+
+    app.locals({
+        title: 'Leslie’s & Eric’s Wedding'
+    });
 };
