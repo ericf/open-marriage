33c196531d3c88786471530932497815427ba5dd
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-01-27 20:04:14 -0500

    Finish adding events to timeline.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,20 @@
 YUI.add('lew-app', function (Y) {
 
+Y.later(0, Y.config.win, 'scrollTo', [0, 0]);
+
 Y.all('[data-map]').each(function (mapNode) {
-    mapbox.auto(mapNode.getDOMNode(), mapNode.getData('map'), function (map) {
-        if (window.devicePixelRatio >= 2) {
-            map.tileSize = {x: 128, y: 128};
-        }
+    mapbox.load(mapNode.getData('map'), function (data) {
+        var map = mapbox.map(mapNode.getDOMNode(), [
+            data.layer,
+            data.markers
+        ], null, [
+            MM.DoubleClickHandler(),
+            MM.DragHandler()
+        ]);
+
+        map.tileSize = {x: 128, y: 128};
+        map.ui.zoomer.add();
+        map.centerzoom(data.center, data.zoom);
     });
 });
 
