b5f00a070951fcad2046872e97e6437edc505d90
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-03-09 23:28:52 -0500

    Only make maps hi-res on hi-res desplays

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
 YUI.add('lew-app', function (Y) {
 
-Y.later(0, Y.config.win, 'scrollTo', [0, 0]);
+    var win      = Y.config.win,
+        isRetina = win.devicePixelRatio >= 2;
+
+Y.later(0, win, 'scrollTo', [0, 0]);
 
 Y.all('[data-map]').each(function (mapNode) {
     mapbox.load(mapNode.getData('map'), function (data) {
@@ -12,7 +15,10 @@ Y.all('[data-map]').each(function (mapNode) {
             MM.DragHandler()
         ]);
 
-        map.tileSize = {x: 128, y: 128};
+        if (isRetina) {
+            map.tileSize = {x: 128, y: 128};
+        }
+
         map.ui.zoomer.add();
         map.centerzoom(data.center, data.zoom);
     });
