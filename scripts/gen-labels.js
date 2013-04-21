e2c559a4e1239ac580814ac9fc53fe03ae131193
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-04-21 02:54:34 -0400

    Add `loadInvitations()` to Invitations Service, update gen-labels script

--- scripts/gen-labels.js
+++ scripts/gen-labels.js
@@ -1,18 +1,35 @@
 #!/usr/bin/env node
 
-var async  = require('async'),
-    csv    = require('csv'),
-    path   = require('path'),
-    pg     = require('pg'),
+var csv  = require('csv'),
+    path = require('path'),
 
-    config = require('../config'),
-    output = process.argv[2],
+    invs   = require('../lib/services/invitations'),
+    output = process.argv[2];
 
-    INVITATIONS       = 'SELECT * FROM invitations ORDER BY id',
-    INVITATION_GUESTS = 'SELECT * FROM guests WHERE invitation_id=$1 AND is_plusone=false';
+if (output) {
+    output = path.resolve(output);
+} else {
+    console.error('No output path provided!');
+    process.exit(1);
+}
 
-function createLabel(invitation, guests) {
-    var names = guests.map(function (guest) {
+console.log('Querying database...');
+invs.loadInvitations(function (err, invitations) {
+    if (err) { throw err; }
+
+    var labels = invitations.map(createLabel);
+
+    console.log('Writing ' + labels.length + ' labels to CSV file: ' + output);
+    writeCSV(labels, output, function () {
+        console.log('Done!');
+        process.exit();
+    });
+});
+
+function createLabel(invitation) {
+    var names = invitation.guests.filter(function (guest) {
+        return !guest.is_plusone;
+    }).map(function (guest) {
         return guest.title.trim() + ' ' + guest.name.trim();
     });
 
@@ -33,38 +50,3 @@ function writeCSV(labels, output, callback) {
         .to.path(output, options)
         .on('close', callback);
 }
-
-function processInvitations(db, results, callback) {
-    async.map(results.rows, function (invitation, callback) {
-        db.query(INVITATION_GUESTS, [invitation.id], function (err, results) {
-            if (err) { return callback(err); }
-            callback(null, createLabel(invitation, results.rows));
-        });
-    }, callback);
-}
-
-if (output) {
-    output = path.resolve(output);
-} else {
-    console.error('No output path provided!');
-    process.exit(1);
-}
-
-console.log('Querying database...');
-pg.connect(config.database, function (err, db, done) {
-    if (err) { throw err; }
-
-    async.waterfall([
-        db.query.bind(db, INVITATIONS),
-        processInvitations.bind(null, db)
-    ], function (err, labels) {
-        if (err) { throw err; }
-        done();
-
-        console.log('Writing ' + labels.length + ' labels to CSV file: ' + output);
-        writeCSV(labels, output, function () {
-            console.log('Done!');
-            process.exit();
-        });
-    });
-});
