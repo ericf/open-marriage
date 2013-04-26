5f19ce752a5cd2c44ea2031636925032719bf814
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-04-26 10:46:05 -0400

    Add RSVP page to the menu and update placeholder text

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.locals({
     nav: [
         {id: 'wedding',   url: '/wedding/',   label: 'Wedding'},
         {id: 'logistics', url: '/logistics/', label: 'Logistics'},
-        {id: 'registry',  url: '/registry/',  label: 'Registry'}
+        {id: 'registry',  url: '/registry/',  label: 'Registry'},
+        {id: 'rsvp',      url: '/rsvp/',      label: 'RSVP'}
     ],
 
     subnav: {
