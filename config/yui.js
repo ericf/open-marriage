c5fa08a9015e5d608325a89366932ff93ab5ecd7
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-05-04 17:39:47 -0400

    Update YUI to 3.10.0

--- config/yui.js
+++ config/yui.js
@@ -1,55 +1,52 @@
 var isProduction = process.env.NODE_ENV === 'production',
     version      = require('../package').version,
 
-    YUI_VERSION = '3.9.1';
+    YUI_VERSION = '3.10.0';
 
-module.exports = {
-    version: YUI_VERSION,
+exports.version = YUI_VERSION;
+exports.config  = {
+    combine: isProduction,
+    filter : isProduction ? 'min' : 'raw',
+    root   : YUI_VERSION + '/',
 
-    config: {
-        combine: isProduction,
-        filter : isProduction ? 'min' : 'raw',
-        root   : YUI_VERSION + '/',
+    modules: {
+        'mapbox-css': 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css',
 
-        modules: {
-            'mapbox-css': 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css',
+        'mapbox': {
+            fullpath: 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js',
+            requires: ['mapbox-css']
+        }
+    },
 
-            'mapbox': {
-                fullpath: 'http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js',
-                requires: ['mapbox-css']
-            }
-        },
+    groups: {
+        'app': {
+            combine  : isProduction,
+            comboBase: '/combo/' + version + '?',
+            base     : '/',
+            root     : '/',
 
-        groups: {
-            'app': {
-                combine  : isProduction,
-                comboBase: '/combo/' + version + '?',
-                base     : '/',
-                root     : '/',
+            modules: {
+                'hide-address-bar': {
+                    path: 'vendor/hide-address-bar.js'
+                },
 
-                modules: {
-                    'hide-address-bar': {
-                        path: 'vendor/hide-address-bar.js'
-                    },
+                'le-wedding': {
+                    path: 'js/wedding.js',
+                    requires: ['le-main', 'le-maps', 'event-resize', 'graphics']
+                },
 
-                    'le-wedding': {
-                        path: 'js/wedding.js',
-                        requires: ['le-main', 'le-maps', 'event-resize', 'graphics']
-                    },
+                'le-home': {
+                    use: ['le-main', 'le-maps']
+                },
 
-                    'le-home': {
-                        use: ['le-main', 'le-maps']
-                    },
+                'le-main': {
+                    path    : 'js/main.js',
+                    requires: ['node-base', 'hide-address-bar']
+                },
 
-                    'le-main': {
-                        path    : 'js/main.js',
-                        requires: ['node-base', 'hide-address-bar']
-                    },
-
-                    'le-maps': {
-                        path    : 'js/maps.js',
-                        requires: ['node-base', 'mapbox']
-                    }
+                'le-maps': {
+                    path    : 'js/maps.js',
+                    requires: ['node-base', 'mapbox']
                 }
             }
         }
