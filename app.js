f56d3dfe72ecfdd99dc538cfd3645d0409dcfd98
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-09-16 23:42:46 -0400

    Switch order the RSVP routes are registered in

--- app.js
+++ app.js
@@ -110,10 +110,10 @@ app.get('/registry/', routes.render('registry'));
 
 app.get( '/rsvp/',                       routes.rsvp.pub, routes.rsvp.edit);
 app.post('/rsvp/',                       routes.rsvp.resend);
+app.get( '/rsvp/:invitation_key',        routes.rsvp.login);
 app.get( '/rsvp/brunch/',                routes.rsvp.brunch);
 app.post('/rsvp/brunch/',                routes.rsvp.brunch);
 app.get( '/rsvp/brunch/:invitation_key', routes.rsvp.login);
-app.get( '/rsvp/:invitation_key',        routes.rsvp.login);
 
 app.all( '/invitations/:invitation/*',       middleware.auth.ensureInvitation);
 app.get( '/invitations/:invitation/',        routes.invitations.read);
